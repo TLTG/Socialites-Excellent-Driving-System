ba6a886fe6ede4cc78671e34466696cc201f7cd0
Graciella Tatel <31510067+ellaaatrix@users.noreply.github.com>
2018-04-25 16:29:07 +0800

    Made comments (#9)
    * Made comments
    
    * Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,5 @@
 /* 
-    errorHandler.js, from it name itself hahaha. 
+    errorHandler.js, from its name itself hahaha. 
     Ginawa ko to para mahandle lahat ng error that might happen.
     May default handler naman ng error ang express pero di sa specific ng system natin.
 */
@@ -8,9 +8,9 @@ exports.error500 = function(err, req, res){
     console.error("[SEVER] Error occur. Check error.log for information.");
     errorLog(err.stack);
     if(req.method == "POST"){
-        return res.status(500).send({error: 500, detail: "Internal Server Error, Sorry for inconvience we'll fix it soon."});
+        return res.status(500).send({error: 500, detail: "Internal Server Error. Sorry for the inconvience. We'll fix it soon."});
     }else{
-        return res.status(500).send("<h1>Internal Server Error, Sorry for inconvience we'll fix it soon.</h1>");
+        return res.status(500).send("<h1>Internal Server Error. Sorry for the inconvience. We'll fix it soon.</h1>");
     }
 }
 
@@ -29,4 +29,5 @@ var errorLog = function(err){
     });
 }
 
-//SD:  I suggest doing a View for this errors, para di mukhang plain and halatang error. :D
+//SD:  I suggest doing a View for these errors, para di mukhang plain and halatang error. :D
+//RE: Noted po! So lalagyan ko to ng sweetalert. Di ko muna tanggalin tong comment since di ko pa nagagawa. Let's discuss this further on sat, Apr 28, 2018 :)
